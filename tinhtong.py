5487079923ee4588cecc397ad6c61ace85cf157e
Trần Nguyễn Tuấn Anh <43093213+dopaemon@users.noreply.github.com>
2022-02-22 16:03:30 +0700

    Create tinhtong.py

---
+++ tinhtong.py
@@ -0,0 +1,8 @@
+a = int(input('Nhập giá trị a: '))
+
+b = int(input('Nhập giá trị b: '))
+
+c = a + b
+
+print("bạn vừa nhập a = " + str(a) + " và b = " + str(b))
+print("Kết quả của a + b = " + str(c))
